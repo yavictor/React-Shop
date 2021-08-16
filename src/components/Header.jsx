bf2c3902d5dfcfcd5160e3b543cdcd0dfa89d347
yavictor <yavictor37@yandex.ru>
2021-08-16 10:36:50 +0300

    Initialised repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <nav>
+      <div className="orange nav-wrapper">
+        <a href="#!" class="brand-logo center">React-Shop</a>
+        <ul className="left hide-on-med-and-down">
+          <li><a href="#!">link</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
---
+++ src/components/Shop.jsx
@@ -0,0 +1,5 @@
+function Shop() {
+  return <main className="container content">Items</main>
+}
+
+export {Shop};
